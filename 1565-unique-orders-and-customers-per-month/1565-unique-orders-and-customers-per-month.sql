1e033ca972337e2031a9969ad3c695b6e2be9f13
Boris <91817177+bveits92@users.noreply.github.com>
2023-12-26 20:28:15 -0700

    Time: 1082 ms (68.15%), Space: 0B (100%) - LeetHub

--- 1565-unique-orders-and-customers-per-month/1565-unique-orders-and-customers-per-month.sql
+++ 1565-unique-orders-and-customers-per-month/1565-unique-orders-and-customers-per-month.sql
@@ -2,7 +2,7 @@
 SELECT 
     DATE_FORMAT(order_date, '%Y-%m') AS month,
     COUNT(DISTINCT order_id) AS order_count,
-    COUNT(DISTINCT CASE WHEN invoice > 20 THEN customer_id END) AS customer_count
+    COUNT(DISTINCT  customer_id) AS customer_count
 FROM Orders
 WHERE invoice > 20
 GROUP BY month
